03a26d0419d3b8f2d13bdfc7c774050584e2c187
autodios <31591644+autodios@users.noreply.github.com>
2022-06-15 12:18:04 +0200

    Update script.js

--- dist/script.js
+++ dist/script.js
@@ -36,7 +36,8 @@ iframeTest.id = "audio";
 iframeTest.style.cssText += "display:none;";
 document.body.appendChild(iframeTest);
 
-const audio = new Audio();
+var audio = document.createElement('video');
+//const audio = new Audio();
 audio.src = "./COMOTODO.mp3";
 audio.controls = true;
 audio.allow = 'autoplay';
@@ -46,14 +47,6 @@ audio.id = "player";
 audio.style.cssText += "display:none;";
 document.body.appendChild(audio);
 
-//var audio = document.createElement('video');
-//audio.src = "./COMOTODO.mp3";
-//audio.controls = true;
-//audio.allow = 'autoplay';
-//audio.autoplay = true;
-//audio.crossOrigin = "anonymous";
-//audio.id = "player";
-//document.body.appendChild(audio);
 
 
 const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
